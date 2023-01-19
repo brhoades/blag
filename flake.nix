6aba5188963fb1af12c789738201e464683cc222
Billy J Rhoades II <me@brod.es>
2023-01-19 15:34:31 +0000

    default.nix -> flake.nix. ruby 2.6 -> ruby 3.1

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "blag shell and devenv";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: let
+    pkgsFor = system: import nixpkgs {
+      inherit system;
+    }; in (flake-utils.lib.eachDefaultSystem (system: {
+      # envrc
+      devShells.default = with (pkgsFor system); mkShell {
+        buildInputs = [
+          ruby_3_1
+          nodejs_latest
+          yarn
+        ];
+      };
+    }));
+}
+
